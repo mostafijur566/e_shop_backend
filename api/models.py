cc8a943f12f88a9b31c06d8c86a6c0a70d57ac6f
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-08-15 20:41:16 +0600

    model added

--- api/models.py
+++ api/models.py
@@ -1,1 +1,152 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+# Create your models here.
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("users must have an email address")
+        if not username:
+            raise ValueError("users must have a username")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(max_length=60, unique=True)
+    username = models.CharField(max_length=30, unique=True, primary_key=True)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = MyAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class CustomerDetails(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(max_length=60)
+    image = models.ImageField()
+    billing_address = models.TextField()
+    shipping_address = models.TextField()
+    phone = models.CharField(max_length=14)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class ProductCategories(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    category_name = models.CharField(max_length=60, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    descriptions = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE, null=False, blank=False)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class OrderDetails(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_id.name
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class Order(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    orderDetails = models.ManyToManyField(OrderDetails)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_at']
+
+
+class OrderHistory(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False, blank=False)
+    payment_status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_at']
