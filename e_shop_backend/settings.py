ec440d6f7eadb7277da040894e5d4ba6562ab9bc
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-08-18 01:25:43 +0600

    commit

--- e_shop_backend/settings.py
+++ e_shop_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5p#$qd1!bw-r9y%p(m)#imyez$piy#i)!m=vei59!o^4-7dzvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['e-shop-system.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = "api.Account"
 
